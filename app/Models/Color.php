a31af1e850ee5c82b8b9df2c804cfb0a38ceb27c
irman99 <irman.isic@edu.fit.ba>
2024-05-15 22:56:57 +0200

    Fixed models Q13

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Color extends Model
 {
     use HasFactory;
+
+
     protected $table ='colors';
     protected $fillable=[
         'color_name',
