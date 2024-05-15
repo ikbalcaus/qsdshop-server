97e3f14fde582fe2cafff559d00e06914cc295d2
irman99 <irman.isic@edu.fit.ba>
2024-05-16 01:26:34 +0200

    Modified models and completed relations Q13

--- app/Models/Discount.php
+++ app/Models/Discount.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Discount extends Model
 {
     use HasFactory;
+    protected $table = "discount";
+    protected $fillable = [
+        "DiscountName",
+        "DiscountValue",
+        "ValidFrom",
+        "ValidTo"
+        ];
+
+    public function productDiscounts():BelongsTo{
+        return $this->belongsTo(ProductDiscount::class,'discount_id','id');
+    }
+
 }
