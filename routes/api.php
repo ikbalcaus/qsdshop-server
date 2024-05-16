f392ce0868ffd4f95c5e3cb1e05acfb244133939
irman99 <irman.isic@edu.fit.ba>
2024-05-16 02:39:23 +0200

    Started implementing Auth endpoint Q14

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/register','AuthController@register');
+Route::post('/login','AuthController@login');
+Route::post('/logout','AuthController@logout');
+Route::post('/requestValidationKey','AuthController@requestValidationKey');
+Route::post('/resetPassword','AuthController@resetPassword');
+Route::post('/refresh','AuthController@refresh');
+Route::post('/changePassword','AuthController@changePassword');
+
