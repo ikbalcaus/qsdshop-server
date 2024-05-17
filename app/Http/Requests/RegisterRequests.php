9ad57f9664ead8d1420d6f2f067eb06129aed727
irman99 <irman.isic@edu.fit.ba>
2024-05-17 12:23:33 +0200

    Finished POST register Q14

---
+++ app/Http/Requests/RegisterRequests.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequests extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "email"=> "required|email",
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'password'=>'required|string|min:8',
+            'city' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'zip_code' => 'required|string|max:255',
+            'phone' => 'required|string|regex:/^[0-9]+$/|min:10|max:15',
+            'role'=>'required|in:Superadmin,Admin,Uditor',
+            'status'=>'required|boolean'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,12 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/register','AuthController@register');
+Route::post('/register',[AuthController::class,'register']);
 Route::post('/login','AuthController@login');
 Route::post('/logout','AuthController@logout');
 Route::post('/requestValidationKey','AuthController@requestValidationKey');
