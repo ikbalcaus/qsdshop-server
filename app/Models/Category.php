b55ba98e093e085dcb3a819ba293ddafd573a58e
Adnan <adnan.voloder9@gmail.com>
2024-06-03 11:53:01 +0200

    Added match against method for searching products

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,14 +6,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+
 class Category extends Model
 {
     use HasFactory;
-    protected $table='category';
-    protected $fillable=[
+
+    protected $table = 'category';
+    protected $fillable = [
         'name'
     ];
-    public function productCategory():BelongsTo{
+
+    public function productCategory(): BelongsTo
+    {
         return $this->belongsTo(ProductCategory::class);
     }
 }
